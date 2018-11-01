9842c27eefdbfe360746d662edf59ea3af6454d3
xihaoliang <xihao.liang@ihandysoft.com>
2018-11-01 11:09:34 +0800

    n-gram tf-idf added

--- algo/svm.py
+++ algo/svm.py
@@ -5,13 +5,19 @@ import importlib
 import yaml
 import numpy as np
 from sklearn import svm
+from scipy.sparse import hstack
 from dataset.common.const import *
 from dataset.common.load import *
 from algo.model.const import *
 from algo.lib.evaluate import basic_evaluate
 from algo.lib.common import print_evaluation
+from sklearn.decomposition import TruncatedSVD as SVD
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from nlp.lexicon_feat import extract_tf
 
 FEATS = 'feats'
+TF_IDF = 'tf-idf'
+TF = 'tf'
 
 
 class Config(object):
@@ -72,12 +78,119 @@ def main(dataset_key, label_version=None, config_path='config_svm.yaml'):
     else:
         class_weight = None
 
+    #tf = extract_tf(dataset_key, text_version='ek')
+
     clf = svm.SVC(class_weight=class_weight)
-    clf.fit(X=datasets[TRAIN][FEATS], y=datasets[TRAIN][LABEL_GOLD])
+    #X = hstack([datasets[TRAIN][FEATS], tf[TRAIN]])
+    X = datasets[TRAIN][FEATS]
+    clf.fit(X=X, y=datasets[TRAIN][LABEL_GOLD])
 
     for mode in [TRAIN, TEST]:
-        labels_predict = clf.predict(X=datasets[mode][FEATS])
+        #X = hstack([datasets[mode][FEATS], tf[mode]])
+        X = datasets[mode][FEATS]
+        labels_predict = clf.predict(X=X)
         labels_gold = datasets[mode][LABEL_GOLD]
+        res = basic_evaluate(gold=labels_gold, pred=labels_predict, pos_label=pos_label)
+
+        print(mode)
+        print_evaluation(res)
+        print()
+
+
+@commandr.command
+def main2(dataset_key, label_version=None, config_path='config_svm.yaml'):
+    """
+    python algo/svm.py main semeval2018_task3 A
+
+    :param dataset_key:
+    :param label_version:
+    :param config_path:
+    :return:
+    """
+    pos_label = None
+    if dataset_key == 'semeval2018_task3' and label_version == 'A':
+        pos_label = 1
+
+    config_data = yaml.load(open(config_path))
+    train_config = Config(data=config_data)
+
+    data_config = getattr(importlib.import_module('dataset.{}.config'.format(dataset_key)), 'config')
+    datasets = load_dataset(data_config, train_config, label_version)
+
+    if train_config.use_class_weights:
+        class_weight = 'balanced'
+    else:
+        class_weight = None
+
+    tf = extract_tf(dataset_key, text_version='ek')
+
+    clf = svm.SVC(class_weight=class_weight)
+    X = hstack([datasets[TRAIN][FEATS], tf[TRAIN]])
+    clf.fit(X=X, y=datasets[TRAIN][LABEL_GOLD])
+
+    for mode in [TRAIN, TEST]:
+        X = hstack([datasets[mode][FEATS], tf[mode]])
+        labels_predict = clf.predict(X=X)
+        labels_gold = datasets[mode][LABEL_GOLD]
+        res = basic_evaluate(gold=labels_gold, pred=labels_predict, pos_label=pos_label)
+
+        print(mode)
+        print_evaluation(res)
+        print()
+
+
+@commandr.command
+def tf_idf(dataset_key, text_version, label_version=None, use_class_weights=True):
+    """
+    python algo/svm.py tf_idf semeval2018_task3 -t ek -l A
+    """
+    data_config = getattr(importlib.import_module('dataset.{}.config'.format(dataset_key)), 'config')
+    pos_label = None
+    if dataset_key == 'semeval2018_task3' and label_version == 'A':
+        pos_label = 1
+
+    datasets = dict()
+    for mode in [TRAIN, TEST]:
+        datasets[mode] = {
+            TEXT: load_text_list(data_config.path(mode, TEXT, text_version)),
+            LABEL: load_label_list(data_config.path(mode, LABEL, label_version))
+        }
+
+    decomposition = True
+    pcas = {
+        TF: SVD(n_components=1000),
+    }
+    vectorizers = {
+        #TF_IDF: TfidfVectorizer(max_df=0.5),
+        TF: CountVectorizer(ngram_range=(1, 3), min_df=10, )
+    }
+
+    for key, vectorizer in vectorizers.items():
+        feat = vectorizer.fit_transform(datasets[TRAIN][TEXT])
+        if decomposition:
+            feat = pcas[key].fit_transform(X=feat)
+        datasets[TRAIN][key] = feat
+
+        feat = vectorizer.transform(datasets[TEST][TEXT])
+        if decomposition:
+            feat = pcas[key].transform(X=feat)
+        datasets[TEST][key] = feat
+
+    if use_class_weights:
+        class_weight = 'balanced'
+    else:
+        class_weight = None
+
+    clf = svm.SVC(class_weight=class_weight)
+    #X = hstack([datasets[TRAIN][k] for k in vectorizers.keys()])
+    X = datasets[TRAIN][TF]
+    clf.fit(X=X, y=datasets[TRAIN][LABEL])
+
+    for mode in [TRAIN, TEST]:
+        #X = hstack([datasets[mode][k] for k in vectorizers.keys()])
+        X = datasets[mode][TF]
+        labels_predict = clf.predict(X=X)
+        labels_gold = datasets[mode][LABEL]
         res = basic_evaluate(gold=labels_gold, pred=labels_predict, pos_label=pos_label)
 
         print(mode)
--- nlp/lexicon_feat.py
+++ nlp/lexicon_feat.py
@@ -1,8 +1,24 @@
 # -*- coding: utf-8 -*-
 import commandr
 import importlib
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from dataset.common.const import *
 from dataset.common.load import *
+
+
+def extract_tf(dataset_key, text_version):
+    #vectorizer = CountVectorizer()
+    vectorizer = TfidfVectorizer(max_df=0.5)
+    data_config = getattr(importlib.import_module('dataset.{}.config'.format(dataset_key)), 'config')
+
+    texts = dict()
+    for mode in [TRAIN, TEST]:
+        texts[mode] = load_text_list(data_config.path(mode, TEXT, text_version))
+
+    feat = dict()
+    feat[TRAIN] = vectorizer.fit_transform(texts[TRAIN])
+    feat[TEST] = vectorizer.transform(texts[TEST])
+    return feat
 
 
 @commandr.command
@@ -11,3 +27,19 @@ def tf_idf(dataset_key, text_version):
     for mode in [TRAIN, TEST]:
         text_path = data_config.path(mode, text_version)
         tokenized_list = load_tokenized_list(text_path)
+
+    datasets = dict()
+    for mode in [TRAIN, TEST]:
+        datasets[mode] = {
+            TEXT: load_text_list(data_config.path(mode, TEXT, text_version)),
+            LABEL: load_label_list(data_config.path(mode, LABEL, label_version))
+        }
+
+    vectorizers = {
+        #TF_IDF: TfidfVectorizer(),
+        TF: CountVectorizer()
+    }
+
+    for key, vectorizer in vectorizers.items():
+        datasets[TRAIN][key] = vectorizer.fit_transform(datasets[TRAIN][TEXT])
+        datasets[TEST][key] = vectorizer.transform(datasets[TEST][TEXT])

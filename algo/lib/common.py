21492df2a6855b218f1e22ee300b886967c8a703
xihaoliang <xihao.liang@ihandysoft.com>
2019-01-07 21:34:35 +0800

    update

--- algo/lib/common.py
+++ algo/lib/common.py
@@ -45,10 +45,47 @@ def load_lookup_table(w2v_model_path, vocabs):
 
     table_mean = lookup_table_pretrained.mean(axis=0)
     table_std = (lookup_table_pretrained - table_mean).std(axis=0)
+
     lookup_table_patch = np.random.normal(
         table_mean, table_std, (n_not_supported, w2v_model.dim))
 
     lookup_table = np.concatenate([lookup_table_pretrained, lookup_table_patch])
+
+    vocab_list += not_supported_vocabs
+    vocab_id_mapping = {_vocab: _i for _i, _vocab in enumerate(vocab_list)}
+
+    embedding_dim = w2v_model.dim
+    return lookup_table, vocab_id_mapping, embedding_dim
+
+
+def load_lookup_table2(w2v_model_path, vocabs):
+    """
+    取模型的所有词向量, 以及为train中出现但模型中没有的词随机生成词向量
+    :param w2v_model_path: string
+    :param vocabs: list of string, 需要确保在的字典, 不在模型中的话则随机生成
+    :return:
+        lookup_table: np.array, [VOCAB_SIZE, EMBEDDING_DIM]
+        vocab_id_mapping: dict, <VOCAB, ID>
+        embedding_dim: int
+    """
+    w2v_model = PlainModel(w2v_model_path)
+    vocab_list = ['ZERO_VEC', ] + list(w2v_model.index.keys())
+
+    not_supported_vocabs = list(filter(lambda _vocab: _vocab not in set(vocab_list), vocabs))
+    n_not_supported = len(not_supported_vocabs)
+
+    lookup_table_pretrained = np.asarray([w2v_model.get(_vocab) for _vocab in vocab_list])
+    print('n supported: {}'.format(lookup_table_pretrained.shape[0]))
+    print('n not supported: {}'.format(n_not_supported))
+
+    table_mean = lookup_table_pretrained.mean(axis=0)
+    table_std = (lookup_table_pretrained - table_mean).std(axis=0)
+    zero_vec = np.zeros((1, w2v_model.dim))
+
+    lookup_table_patch = np.random.normal(
+        table_mean, table_std, (n_not_supported, w2v_model.dim))
+
+    lookup_table = np.concatenate([zero_vec, lookup_table_pretrained, lookup_table_patch])
 
     vocab_list += not_supported_vocabs
     vocab_id_mapping = {_vocab: _i for _i, _vocab in enumerate(vocab_list)}

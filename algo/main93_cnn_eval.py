8e86a24e3a4d16c6ecbba3ed610f61bad967d84c
xihaoliang <xihao.liang@ihandysoft.com>
2019-01-11 01:23:30 +0800

    update

--- algo/main93_cnn_eval.py
+++ algo/main93_cnn_eval.py
@@ -412,6 +412,9 @@ def train(text_version='ek', label_version=None, config_path='config93_naive.yam
             json.dump(res, open(data_config.output_path(output_key, mode, EVALUATION), 'w'))
             print()
 
+    test_score_list = map(lambda _item: _item['f1'], eval_history[TEST])
+    print('best test f1 reached: {}'.format(max(test_score_list)))
+
     print('OUTPUT_KEY: {}'.format(output_key))
 
 

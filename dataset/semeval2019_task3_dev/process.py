5fce991e3b154077bb350fad013f98d2df11c673
xihaoliang <xihao.liang@ihandysoft.com>
2019-01-19 20:49:04 +0800

    update

--- dataset/semeval2019_task3_dev/process.py
+++ dataset/semeval2019_task3_dev/process.py
@@ -28,6 +28,27 @@ class Processor(object):
         return dataset
 
     @classmethod
+    def load_origin_no_labels(cls, path):
+        dataset = list()
+        with open(path, 'r') as file_obj:
+            file_obj.readline()
+            for line in file_obj:
+                line = line.strip()
+                if line == '':
+                    continue
+                parts = line.split('\t')
+                if not len(parts) == 4:
+                    print(parts)
+                    raise Exception
+                turn_1 = parts[1].strip()
+                turn_2 = parts[2].strip()
+                turn_3 = parts[3].strip()
+
+                sample = (turn_1, turn_2, turn_3)
+                dataset.append(sample)
+        return dataset
+
+    @classmethod
     def load_origin_train(cls):
         return cls.load_origin(config.path_train)
 
@@ -37,44 +58,8 @@ class Processor(object):
 
     @classmethod
     def load_origin_dev_no_labels(cls):
-        path = config.path_dev_no_labels
-        dataset = list()
-        with open(path, 'r') as file_obj:
-            file_obj.readline()
-            for line in file_obj:
-                line = line.strip()
-                if line == '':
-                    continue
-                parts = line.split('\t')
-                if not len(parts) == 4:
-                    print(parts)
-                    raise Exception
-                turn_1 = parts[1].strip()
-                turn_2 = parts[2].strip()
-                turn_3 = parts[3].strip()
-
-                sample = (turn_1, turn_2, turn_3)
-                dataset.append(sample)
-        return dataset
+        return cls.load_origin_no_labels(config.path_dev_no_labels)
 
     @classmethod
     def load_origin_test_no_labels(cls):
-        path = config.path_test_no_labels
-        dataset = list()
-        with open(path, 'r') as file_obj:
-            file_obj.readline()
-            for line in file_obj:
-                line = line.strip()
-                if line == '':
-                    continue
-                parts = line.split('\t')
-                if not len(parts) == 4:
-                    print(parts)
-                    raise Exception
-                turn_1 = parts[1].strip()
-                turn_2 = parts[2].strip()
-                turn_3 = parts[3].strip()
-
-                sample = (turn_1, turn_2, turn_3)
-                dataset.append(sample)
-        return dataset
+        return cls.load_origin_no_labels(config.path_test_no_labels)

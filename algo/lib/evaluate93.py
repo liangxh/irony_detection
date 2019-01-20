5ab0b2c19ad60df0dd2624ecd3345ed47eba5fcf
xihaoliang <xihao.liang@ihandysoft.com>
2019-01-20 16:39:36 +0800

    update

--- algo/lib/evaluate93.py
+++ algo/lib/evaluate93.py
@@ -118,7 +118,7 @@ def basic_evaluate(gold, pred):
     microF1 = (2 * microRecall * microPrecision) / (microPrecision + microRecall) if (microPrecision + microRecall) > 0 else 0
     accuracy = float(match.sum()) / n_sample
 
-    test_score = estimate_test_score(matrix)
+    test_score = estimate_test_score(matrix) if dim == 4 else None
 
     n_pred_0 = float(predictions[:, 0].sum())
     n_gold_0 = float(ground[:, 0].sum())

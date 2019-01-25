8af5cb27f12000fe8964105bf3d297c7e69296ec
xihaoliang <xihao.liang@ihandysoft.com>
2019-01-25 11:55:17 +0800

    update

--- algo/im93.py
+++ algo/im93.py
@@ -353,8 +353,9 @@ def others_out(filename, thr, output_file, config_path='e93.yaml'):
     with open(output_file, 'w') as file_obj:
         for i, (d, v_others, v_tri) in enumerate(zip(dataset, votes_others, votes_tri)):
             if d[-1] == 0 and v_others <= thr:
-                file_obj.write('{}\t{}\t{}\t{}\t{} ({}->{})\n'.format(
-                    i, d[0], d[1], d[2], d[-1], v_others, v_tri
+                new_label = label_str[argmax(v_tri)]
+                file_obj.write('{}\t{}\t{}\t{}\t{} ({} {})\n'.format(
+                    i, d[0], d[1], d[2], d[-1], new_label, v_tri
                 ))
 
 

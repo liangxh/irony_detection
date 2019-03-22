9e39ee871e1ef29ac2f414af27dac60ba0e618f6
xihaoliang <xihao.liang@ihandysoft.com>
2019-03-22 17:11:41 +0800

    update

---
+++ algo/ef83.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import commandr
+import numpy as np
+import yaml
+from algo.model.const import *
+from dataset.semeval2018_task3.config import config as data_config
+from dataset.common.const import *
+from dataset.common.load import *
+from algo.lib.evaluate93 import basic_evaluate
+from algo.lib.common import print_evaluation
+
+MAJORITY_VOTING = 'mv'
+WEIGHTED_MAJORITY_VOTE = 'wmv'
+SOFT_VOTING = 'sv'
+
+
+ANALYSIS = 'analysis'
+WRONG_PREDICT = 'wrong_predict'
+
+
+class Config(object):
+    def __init__(self, data):
+        self.data = data
+
+    @property
+    def components(self):
+        return self.data['components']
+
+    @property
+    def components_b2(self):
+        return self.data['components_b2']
+
+    @property
+    def components_b3(self):
+        return self.data['components_b3']
+
+    @property
+    def others(self):
+        return self.data['others']['components']
+
+    @property
+    def others_enabled(self):
+        return self.data['others']['enabled']
+
+    @property
+    def others_min_vote(self):
+        return self.data['others']['min_vote']
+
+    @property
+    def tri(self):
+        return self.data['tri']['components']
+
+    @property
+    def tri_enabled(self):
+        return self.data['tri']['enabled']
+
+    @property
+    def tri_min_vote(self):
+        return self.data['tri']['min_vote']
+
+
+def argmax(value_list):
+    idx = 0
+    max_value = value_list[idx]
+    for i, value in enumerate(value_list[1:]):
+        if value > max_value:
+            max_value = value
+            idx = i + 1
+    return idx, max_value
+
+
+def combine(output_keys, mode):
+    dim = 0
+    label_lists = list()
+    for output_key in output_keys:
+        path = data_config.output_path(output_key, mode, LABEL_PREDICT)
+        label_list = load_label_list(path)
+        label_lists.append(label_list)
+        dim = max(dim, max(label_list) + 1)
+
+    res = list()
+    for votes in zip(*label_lists):
+        count = [0] * dim
+        for v in votes:
+            count[v] += 1
+        vote, vote_count = argmax(count)
+        res.append((vote, vote_count))
+    return res
+
+
+@commandr.command
+def main(config_path='e83.yaml'):
+    """
+    [Usage]
+    python3 -m algo.ensemble93 main -e mv --build-analysis
+
+    :param config_path:
+    :return:
+    """
+    config_data = yaml.load(open(config_path))
+    config = Config(data=config_data)
+
+    for mode in [TRAIN, TEST]:
+        b_result = combine(output_keys=config.components, mode=mode)
+        b_vote = list(map(lambda _item: _item[0], b_result))
+
+        b2_result = combine(output_keys=config.components_b2, mode=mode)
+        b2_vote = list(map(lambda _item: _item[0], b2_result))
+
+        last_vote = list()
+        for b_v, b2_v in zip(b_vote, b2_vote):
+            if b_v == 0:
+                label = 0
+            elif b2_v == 0:
+                label = 1
+            else:
+                label = 2
+            last_vote.append(label)
+
+        b3_result = combine(output_keys=config.components_b3, mode=mode)
+        b3_vote = list(map(lambda _item: _item[0], b3_result))
+
+        labels_predict = list()
+        for last_v, b3_v in zip(last_vote, b3_vote):
+            if last_v != 2:
+                label = last_v
+            elif b3_v == 0:
+                label = 2
+            else:
+                label = 3
+            labels_predict.append(label)
+
+        labels_gold = load_label_list(data_config.path(mode, LABEL, 'B'))
+
+        res = basic_evaluate(gold=labels_gold, pred=labels_predict)
+
+        print(mode)
+        print_evaluation(res)
+        for col in res[CONFUSION_MATRIX]:
+            print(','.join(map(str, col)))
+
+
+if __name__ == '__main__':
+    commandr.Run()
